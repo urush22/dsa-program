7bc81d4c7d79bc7ca76d1c6c74cf5fc938fe22d0
urush22 <146115669+urush22@users.noreply.github.com>
2024-01-14 20:21:47 +0530

    Update a2_q13.cpp

--- a2_q13.cpp
+++ a2_q13.cpp
@@ -13,8 +13,8 @@ int main(){
     int n; 
     cin>>n;
     for(int i=1;i<=n;i++){
-        for(int j=0;j<=i;j++){
-            if(j==0){
+        for(int j=1;j<=i;j++){
+            if(j==1){
                 cout<<1<<"\t";}
            else {cout<<combin(i,j)<<"\t";}
         }
